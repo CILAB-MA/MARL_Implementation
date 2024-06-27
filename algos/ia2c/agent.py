323d75c8287e95d60adf8d619545277817c154db
kevinjeon <kevinjeon119@gmail.com>
2024-06-27 22:56:10 +0900

    Update some codes

--- algos/ia2c/agent.py
+++ algos/ia2c/agent.py
@@ -1,11 +1,13 @@
 import numpy as np
-import torch
+import torch as tr
 import torch.optim as optim
 torch.distributions.categorical.Categorical
 from algos.ia2c.policies import ActorCriticPolicy
 class IA2CAgent:
     def __init__(self, model, env_cfgs, model_cfgs, train_cfgs):
-        self.model = ActorCriticPolicy(model_cfgs)
+        self.models = [ActorCriticPolicy(model_cfgs) for _ in range(env_cfgs['num_agent'])]
+        self.num_agent = env_cfgs['num_agent']
+        self.num_process = env_cfgs['num_process']
         self.env_cfgs = env_cfgs
         self.model_cfgs = model_cfgs
         self.train_cfgs = train_cfgs
@@ -14,5 +16,8 @@ class IA2CAgent:
         pass
 
     def act(self, obs, deterministic=False):
-        action = self.model.act(obs, deterministic=deterministic)
-        return action
+        total_actions = tr.zeros(self.num_process, self.num_agent)
+        for i in range(self.num_agent):
+            action = self.models[i].act(obs, deterministic=deterministic)
+            total_actions[i] = action
+        return total_actions
--- algos/ia2c/train.py
+++ algos/ia2c/train.py
@@ -1,18 +1,29 @@
 from tqdm import tqdm
-from stable_baselines3.common.vec_env import SubprocVecEnv
-import rware
-from algos.ia2c.wrapper import make_env
-
+from algos.ia2c.agent import IA2CAgent
+import torch as tr
+from utils.envs_func import VecRware, RwareWrapper
+from algos.ia2c.utils import RolloutBuffer
 def train(cfgs):
     train_cfgs = cfgs.train_cfgs
     print(train_cfgs)
     env_cfgs = cfgs.env_cfgs
     model_cfgs = cfgs.model_cfgs
     t = 0
-    num_cpu = 2
-    envs = SubprocVecEnv([make_env() for i in range(num_cpu)])
+    envs = VecRware(train_cfgs['num_process'], "rware-tiny-2ag-v1") #agent 2
+    envs = RwareWrapper(envs)
     obss = envs.reset()
+    agent = IA2CAgent(None, env_cfgs, model_cfgs, train_cfgs)
+    storage = RolloutBuffer() # todo: rollout buffer 제작
     for _ in tqdm(range(train_cfgs['total_timesteps'])):
-        pass
+        actions = agent.act(obss) # todo: action categorical 하게 샘플링 하는거 짜기
+        next_obss, rews, dones, infos = envs.step(actions)
+        storage.add(obss, actions, rews, dones, ) # todo: 더 넣을거 추려보기
+        t += 1
+        if t > 0 and t % train_cfgs['rollout_step'] == 0:
+            next_values = agent.get_value(next_obss) # todo: get_value 함수 짜기
+            storage.compute_advantage(next_values, dones) # todo: advantage 계산 짜기
+            agent.update(storage) # todo: rollout buffer 기반 update 함수 짜기
+            storage.clear(next_obss) # todo: storage clear 짜기
+        obss = next_obss
     print('Train completed!!')
 
