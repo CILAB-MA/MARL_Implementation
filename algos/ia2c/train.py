306af3a721724caf9ec0de17552008436868cc6b
kevinjeon <kevinjeon119@gmail.com>
2024-06-28 10:49:50 +0900

    Make RolloutBuffer func add, compute_advantage, and reset

--- algos/ia2c/train.py
+++ algos/ia2c/train.py
@@ -3,7 +3,7 @@ from algos.ia2c.agent import IA2CAgent
 import torch as tr
 from utils.envs_func import VecRware, RwareWrapper
 from algos.ia2c.utils import RolloutBuffer
-def train(cfgs):
+def train(cfgs): # todo: config 맞춰 만들어주기
     train_cfgs = cfgs.train_cfgs
     print(train_cfgs)
     env_cfgs = cfgs.env_cfgs
@@ -13,15 +13,20 @@ def train(cfgs):
     envs = RwareWrapper(envs)
     obss = envs.reset()
     agent = IA2CAgent(None, env_cfgs, model_cfgs, train_cfgs)
-    storage = RolloutBuffer() # todo: rollout buffer 제작
+    storage = RolloutBuffer(num_agent=env_cfgs['num_agent'],
+                            num_obss=train_cfgs['num_obss'],
+                            buffer_size=train_cfgs['rollout_step'],
+                            num_process=train_cfgs['num_process'],
+                            device=train_cfgs['device'])
     for _ in tqdm(range(train_cfgs['total_timesteps'])):
-        actions = agent.act(obss) # todo: action categorical 하게 샘플링 하는거 짜기
+        actions, log_probs = agent.act(obss) # todo: action categorical 하게 샘플링 하는거 짜기 + log_prob 짜내기
+        values = agent.get_value(obss)
         next_obss, rews, dones, infos = envs.step(actions)
-        storage.add(obss, actions, rews, dones, ) # todo: 더 넣을거 추려보기
+        storage.add(obss, actions, rews, dones, values, log_probs)
         t += 1
         if t > 0 and t % train_cfgs['rollout_step'] == 0:
             next_values = agent.get_value(next_obss) # todo: get_value 함수 짜기
-            storage.compute_advantage(next_values, dones) # todo: advantage 계산 짜기
+            storage.compute_advantage(next_values, dones)
             agent.update(storage) # todo: rollout buffer 기반 update 함수 짜기
             storage.clear(next_obss) # todo: storage clear 짜기
         obss = next_obss
--- algos/ia2c/utils.py
+++ algos/ia2c/utils.py
@@ -4,6 +4,49 @@ import numpy as np
 
 class RolloutBuffer(object):
 
-    def __init__(self, num_agent, num_obss):
+    def __init__(self, num_agent, num_obss, buffer_size, num_process, device, gamma=0.99):
+        self.num_agent = num_agent
+        self.num_obss = num_obss
+        self.device = device
+        self.num_process = num_process
+        self.gamma = gamma
+        self.buffer_size = buffer_size
 
-        self.obss = np.zeros(())
+    def reset(self):
+        self.observations = np.zeros((self.buffer_size, self.num_process, self.num_agent, self.num_obss), dtype=np.float32)
+        self.actions = np.zeros((self.buffer_size, self.num_process, self.num_agent), dtype=np.int16)
+        self.rewards = np.zeros((self.buffer_size, self.num_process, self.num_agent), dtype=np.float16)
+        self.dones = np.zeros((self.buffer_size, self.num_process, self.num_agent), dtype=np.float32)
+        self.values = np.zeros((self.buffer_size, self.num_process, self.num_agent), dtype=np.float32)
+        self.advantages = np.zeros((self.buffer_size, self.num_process, self.num_agent), dtype=np.float32)
+        self.log_probs = np.zeros((self.buffer_size, self.num_process, self.num_agent, self.num_action), dtype=np.float32)
+        self.critic_target = np.zeros((self.buffer_size, self.num_process, self.num_agent), dtype=np.float32)
+        self.pos = 0
+
+    def add(self, obss, actions, rewards, dones, values, log_probs):
+        self.observations[self.pos] = obss.copy()
+        self.actions[self.pos] = actions.clone().cpu().numpy() # todo: torch로 넘어오는지 체크
+        self.rewards[self.pos] = np.array(rewards).copy()
+        self.dones[self.pos] = np.array(dones).copy()
+        self.values[self.pos] = values.clone().cpu().numpy()
+        self.log_probs[self.pos] = log_probs.clone().cpu().numpy()
+
+        self.pos += 1
+
+    def compute_advantage(self, next_values, dones):
+        '''
+        rollout step이 꽉차면 돈다
+        '''
+        last_values = next_values.clone().cpu().numpy()
+
+        for step in reversed(range(self.buffer_size)):
+
+            if step == self.buffer_size - 1:
+                next_non_terminal = 1.0 - dones
+                next_values = last_values
+            else:
+                next_non_terminal = 1 - self.dones[step + 1]
+                next_values = self.values[step + 1]
+            self.advantage[step] = self.rewards[step] + self.gamma * (1 - next_non_terminal) * next_values - self.values[step]
+            self.critic_target[step] = self.rewards[step] + self.gamma * (1 - next_non_terminal) * next_values
+
